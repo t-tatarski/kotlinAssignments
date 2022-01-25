f2ce621824b8f07b3e4a96d73e517b51e38ba733
zomal <tehne@tlen.pl>
2022-01-25 23:38:27 +0100

    5 assignment

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,7 @@
+import java.util.*
+import kotlin.random.Random
+import kotlin.random.Random.Default.nextInt
+
 fun main(args: Array<String>) {
     val highScrs= listOf(100,200,304,404,808,994)
     for (ele:Int in highScrs){
@@ -11,6 +15,29 @@ fun main(args: Array<String>) {
     println(airplane.getName())
     print("\n\ta3\n---------\n")
     println(::addSum.invoke(5,10))
+
+    println("menu\n 1 = outloop ")
+
+    do {
+        val input = Scanner(System.`in`)
+        val someWord = input.nextLine()
+        print("select $someWord \n")
+    }while (someWord.trim()!="1")
+
+    for (x in 1..6){
+        print("- $x : "+(1..49).random()+" \n")
+    }
+
+    val longval:Long? = 121212
+    // val intval: Int= longval    conversion error
+
+    val rv:Int =7
+    //equivalent  if(rv<=10 && rv>=1){ //do something }
+    if ( rv in 1..10){
+        print("$rv is lower than 11 and greter than 0")
+    }
+
+
 
 }
 
